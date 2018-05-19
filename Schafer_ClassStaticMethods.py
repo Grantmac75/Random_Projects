b515ce9058dcf3d04db135410dd38a4f8691e48c
Kevin Kurek <kevinkurek@live.com>
2018-05-19 10:27:42 -0700

    Class/Static Methods

--- Schafer_ClassStaticMethods.py
+++ Schafer_ClassStaticMethods.py
@@ -1,27 +1,52 @@
-class payment:
+class Employee:
 
-    raise_amount = 1.03
+    num_of_emps = 0
+    raise_amt = 1.03
 
-    def __init__(self, amount):
-        self.amount = amount
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.pay = pay
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay + self.raise_amt)
 
     def get_amount(self):
         return self.amount
 
     @classmethod
-    def set_class_amount(cls, amount):
+    def set_raise_amount(cls, amount):
         cls.raise_amount = amount
-        return cls.raise_amount
+
+    # Alternative Constructor for String Parsing
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    # Stand Alone Method within Class to check if it's workday
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
 
-p1 = payment(1.5)
-p2 = payment(1.21)
-print(p1.amount)
-print(p1.raise_amount)
-print(p2.amount)
-print(p2.raise_amount)
+emp1 = Employee('Test', 'Employee', 85000)
+print(emp1.email)
+print(emp1.pay)
 
-payment.set_class_amount(1.05)
-print(payment.raise_amount)
-print(p1.raise_amount)
-print(p2.raise_amount)
+emp_str_1 = 'John-Doe-72000'
+new_emp_1 = Employee.from_string(emp_str_1)
+print(new_emp_1.email)
+print(new_emp_1.pay)
+
+import datetime
+my_date = datetime.date(2017, 3, 11)
+print(Employee.is_workday(my_date))
